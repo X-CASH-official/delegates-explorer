ddd75d237e6c094edc5c88fd6b36a0f3519befc9
zachhildreth <zachariahildreth@gmail.com>
2019-07-02 12:34:12 -0700

    Updated API locations
    Updated API locations

--- src/app/services/http-request.service.ts
+++ src/app/services/http-request.service.ts
@@ -5,14 +5,14 @@ import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 export class httpdataservice{
 constructor(private httpClient: HttpClient) {}
 
-SERVER_HOSTNAME_AND_PORT_GET_STATISTICS:string = "http://localhost:8000/getstatistics";
-SERVER_HOSTNAME_AND_PORT_GET_DELEGATES:string = "http://localhost:8000/getdelegates";
-SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_STATISTICS:string = "http://localhost:8000/getdelegatesstatistics";
-SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_INFORMATION:string = "http://localhost:8000/getdelegatesinformation";
-SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_VOTERS_LIST:string = "http://localhost:8000/getdelegatesvoterslist";
-SERVER_HOSTNAME_AND_PORT_GET_ROUND_STATISTICS:string = "http://localhost:8000/getroundstatistics";
-SERVER_HOSTNAME_AND_PORT_GET_NODES_LIST:string = "http://localhost:8000/getnodeslist";
-SERVER_HOSTNAME_AND_PORT_GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION:string = "http://localhost:8000/getpublicaddresspaymentinformation";
+SERVER_HOSTNAME_AND_PORT_GET_STATISTICS:string = "http://localhost/getstatistics";
+SERVER_HOSTNAME_AND_PORT_GET_DELEGATES:string = "http://localhost/getdelegates";
+SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_STATISTICS:string = "http://localhost/getdelegatesstatistics";
+SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_INFORMATION:string = "http://localhost/getdelegatesinformation";
+SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_VOTERS_LIST:string = "http://localhost/getdelegatesvoterslist";
+SERVER_HOSTNAME_AND_PORT_GET_ROUND_STATISTICS:string = "http://localhost/getroundstatistics";
+SERVER_HOSTNAME_AND_PORT_GET_NODES_LIST:string = "http://localhost/getnodeslist";
+SERVER_HOSTNAME_AND_PORT_GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION:string = "http://localhost/getpublicaddresspaymentinformation";
 
 Timer:any;
 
