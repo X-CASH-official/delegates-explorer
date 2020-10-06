9830fbae5a19161bbcde4b3b2fb2dd871756f3b0
miaumiau <dev@xcash.rocks>
2020-10-06 15:35:49 +0200

    rearranged statistics dashcards for furter optimization of font-styles

--- src/app/services/http-request.service.ts
+++ src/app/services/http-request.service.ts
@@ -5,13 +5,13 @@ import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 export class httpdataservice{
 constructor(private httpClient: HttpClient) {}
 
-SERVER_HOSTNAME_AND_PORT_GET_STATISTICS:string = "/delegateswebsitegetstatistics";
-SERVER_HOSTNAME_AND_PORT_GET_DELEGATES:string = "/getdelegates";
-SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_STATISTICS:string = "/getdelegatesstatistics";
-SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_INFORMATION:string = "/getdelegatesinformation";
-SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_VOTERS_LIST:string = "/getdelegatesvoterslist";
-SERVER_HOSTNAME_AND_PORT_GET_ROUND_STATISTICS:string = "/getroundstatistics";
-SERVER_HOSTNAME_AND_PORT_GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION:string = "/getpublicaddresspaymentinformation";
+SERVER_HOSTNAME_AND_PORT_GET_STATISTICS:string = "http://delegates.xcash.foundation/delegateswebsitegetstatistics";
+SERVER_HOSTNAME_AND_PORT_GET_DELEGATES:string = "http://delegates.xcash.foundation/getdelegates";
+SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_STATISTICS:string = "http://delegates.xcash.foundation/getdelegatesstatistics";
+SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_INFORMATION:string = "http://delegates.xcash.foundation/getdelegatesinformation";
+SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_VOTERS_LIST:string = "http://delegates.xcash.foundation/getdelegatesvoterslist";
+SERVER_HOSTNAME_AND_PORT_GET_ROUND_STATISTICS:string = "http://delegates.xcash.foundation/getroundstatistics";
+SERVER_HOSTNAME_AND_PORT_GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION:string = "http://delegates.xcash.foundation/getpublicaddresspaymentinformation";
 XCASH_WALLET_DECIMAL_PLACES_AMOUNT:number = 1000000;
 
 Timer:any;
--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -12,14 +12,14 @@ export class statisticsComponent implements OnInit {
     public_address:string;
     delegatestatistics:string;
     delegateprofileinformation:string;
-        public dashCard1 = [
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 20, text_settings: 20, text: '', settings: false, title: 'MOST TOTAL ROUNDS', icon: 'military_tech' },
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 20, text: 0, settings: true, title: 'MOST TOTAL ROUNDS', icon: 'model_training' }
+    public dashCard1 = [
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 20, text_settings: 20, text: '', settings: false, title: 'MOST BLOCK PRODUCER TOTAL ROUNDS', icon: 'emoji_events' },
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 20, text_settings: 20, text: '', settings: false, title: 'MOST TOTAL ROUNDS', icon: 'military_tech' }
     ];
 
-        public dashCard2 = [
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 20, text_settings: 20, text: '', settings: false, title: 'MOST BLOCK PRODUCER TOTAL ROUNDS', icon: 'emoji_events' },
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 20, text: 0, settings: true, title: 'MOST BLOCK PRODUCER TOTAL ROUNDS', icon: 'find_replace' }
+    public dashCard2 = [
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 20, text: 0, settings: true, title: 'MOST BLOCK PRODUCER TOTAL ROUNDS', icon: 'find_replace' },
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 20, text: 0, settings: true, title: 'MOST TOTAL ROUNDS', icon: 'model_training' }
     ];
 
     constructor(private httpdataservice: httpdataservice) { }
@@ -27,18 +27,16 @@ export class statisticsComponent implements OnInit {
     ngOnInit() {
           // get the data
 	  this.httpdataservice.get_request(this.httpdataservice.SERVER_HOSTNAME_AND_PORT_GET_STATISTICS).subscribe(
-	  (res) =>
-	  {
-            var data = JSON.parse(JSON.stringify(res));
-            this.dashCard1[0].text = data.most_total_rounds_delegate_name;
-            this.dashCard1[1].text = data.most_total_rounds;
-            this.dashCard2[0].text = data.most_block_producer_total_rounds_delegate_name;
-            this.dashCard2[1].text = data.most_block_producer_total_rounds;
-	  },
-	  (error) =>
-          {
-	    Swal.fire("Error","An error has occured","error");
-	  }
+  	  (res) => {
+        var data = JSON.parse(JSON.stringify(res));
+        this.dashCard1[0].text = data.most_block_producer_total_rounds_delegate_name;
+        this.dashCard1[1].text = data.most_total_rounds_delegate_name;
+        this.dashCard2[0].text = data.most_block_producer_total_rounds;
+        this.dashCard2[1].text = data.most_total_rounds;
+  	  },
+  	  (error) =>  {
+  	     Swal.fire("Error","An error has occured","error");
+  	  }
 	  );
-    }
+  }
 }
