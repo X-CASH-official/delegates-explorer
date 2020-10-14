ded210654abc900880563e17910e8b93dd372e67
miaumiau <dev@xcash.rocks>
2020-10-14 14:40:05 +0200

    statistics top 10 to 15, minor css optimizations

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -17,9 +17,9 @@ export class statisticsComponent implements OnInit {
     most_total_rounds_delegate_name:string;
     most_block_producer_total_rounds:number;
     most_total_rounds:number;
-    top10_producer;
-    top10_verifier;
-    top10_ratio;
+    top_producer;
+    top_verifier;
+    top_ratio;
     ratio_data;
 
     public dashCard1 = [
@@ -36,50 +36,51 @@ export class statisticsComponent implements OnInit {
 
     ngOnInit() {
           // get the data
-	  this.HttpdataService.get_request(this.HttpdataService.SERVER_HOSTNAME_AND_PORT_GET_STATISTICS).subscribe(
-  	  (res) => {
-        var data = JSON.parse(JSON.stringify(res));
-        // this.dashCard1[0].text = data.most_block_producer_total_rounds_delegate_name;
-        // this.dashCard1[1].text = data.most_total_rounds_delegate_name;
-        // this.dashCard2[0].text = data.most_block_producer_total_rounds;
-        // this.dashCard2[1].text = data.most_total_rounds;
+  	  this.HttpdataService.get_request(this.HttpdataService.SERVER_HOSTNAME_AND_PORT_GET_STATISTICS).subscribe(
+    	  (res) => {
+          var data = JSON.parse(JSON.stringify(res));
+            // this.dashCard1[0].text = data.most_block_producer_total_rounds_delegate_name;
+            // this.dashCard1[1].text = data.most_total_rounds_delegate_name;
+            // this.dashCard2[0].text = data.most_block_producer_total_rounds;
+            // this.dashCard2[1].text = data.most_total_rounds;
 
-        this.most_block_producer_total_rounds_delegate_name = data.most_block_producer_total_rounds_delegate_name;
-        this.most_total_rounds_delegate_name = data.most_total_rounds_delegate_name;
-        this.most_block_producer_total_rounds = data.most_block_producer_total_rounds;
-        this.most_total_rounds = data.most_total_rounds;
-    	  },
-    	  (error) =>  {
-    	     Swal.fire("Error","An error has occured","error");
-    	  }
-	     );
+            this.most_block_producer_total_rounds_delegate_name = data.most_block_producer_total_rounds_delegate_name;
+            this.most_total_rounds_delegate_name = data.most_total_rounds_delegate_name;
+            this.most_block_producer_total_rounds = data.most_block_producer_total_rounds;
+            this.most_total_rounds = data.most_total_rounds;
+      	  },
+      	  (error) =>  {
+      	     Swal.fire("Error","An error has occured","error");
+      	  }
+       );
 
        this.get_delegates_stats();
     }
+
+
     get_delegates_stats() {
       // get the data
    	  this.HttpdataService.get_request(this.HttpdataService.SERVER_HOSTNAME_AND_PORT_GET_DELEGATES).subscribe(
      	  (res) =>  {
             let data = JSON.parse(JSON.stringify(res));
             let count = 0;
-            let delegate_total_vote_count;
-            let current_delegate_total_vote_count;
+            let top_count = 15;
             let xcash_wallet_decimal_places_amount = this.HttpdataService.XCASH_WALLET_DECIMAL_PLACES_AMOUNT;
 
-            this.top10_producer = data.sort(function(a, b) {
+            this.top_producer = data.sort(function(a, b) {
               return b.block_producer_total_rounds - a.block_producer_total_rounds;
-            }).slice( 0, 10);
+            }).slice( 0, top_count);
 
-            this.top10_verifier = data.sort(function(a, b) {
+            this.top_verifier = data.sort(function(a, b) {
               return b.block_verifier_total_rounds - a.block_verifier_total_rounds;
-            }).slice( 0, 10);
+            }).slice( 0, top_count);
 
             data.map(function(item) {
                item.block_ratio = item.block_producer_total_rounds / item.block_verifier_total_rounds * 100;
             });
-            this.top10_ratio = data.sort(function(a, b) {
+            this.top_ratio = data.sort(function(a, b) {
               return b.block_ratio - a.block_ratio;
-            }).slice( 0, 10);
+            }).slice( 0, top_count);
 
           },
           (error) => {
