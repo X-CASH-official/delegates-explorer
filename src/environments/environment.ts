b22d506e75bed3de7b47990fcdbdf987edc95837
xcash-rocks <dev@xcash.rocks>
2021-03-08 01:03:18 +0100

    fix: set annoucementURL to master repo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     baseURL: "http://delegates.xcash.rocks",
     apiEndPoint: 'http://delegates.xcash.foundation',
-    announcementJSON: 'https://raw.githubusercontent.com/xcash-rocks/delegates-explorer/fix/angular-upgrade/announcement.json'
+    announcementJSON: 'https://raw.githubusercontent.com/X-CASH-official/delegates-explorer/master/announcement.json'
 };
