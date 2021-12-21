2507e7d7f1cc8e0be2ce241c425386fbb39c3071
xcash-rocks <dev@xcash.rocks>
2021-12-21 19:46:19 +0100

    chore: add env fields shortTitle and seedNodes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
     baseURL: "http://delegates.xcash.foundation",
     apiEndPoint: '',
-    announcementJSON: 'https://raw.githubusercontent.com/X-CASH-official/delegates-explorer/master/announcement.json'
+    announcementJSON: 'https://raw.githubusercontent.com/X-CASH-official/delegates-explorer/master/announcement.json',
+    shortTitle: 'Delegates Explorer',
+    seedNodes: [
+                'us1_xcash_foundation',
+                'europe1_xcash_foundation',
+                'europe2_xcash_foundation',
+                'europe3_xcash_foundation',
+                'oceania1_xcash_foundation'
+              ]
 };
